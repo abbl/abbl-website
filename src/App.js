38064dfaf29402548eac5dcff73bb6293a7eb036
abbl <patrykrzucidlo1@gmail.com>
2018-10-03 22:09:45 +0200

    Basic layout implemented

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import avatar from './resources/avatar.png';
+import Particles from 'react-particles-js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App-top-part shadow p-3 mb-5 p-3 mb-5">
+          <div className="App-canvas">
+            <Particles/>
+          </div>
+          <div className="App-avatar-position">
+            <img src={avatar} className="App-avatar" alt="avatar"/>
+          </div>
+        </div>
+        <div className="App-bottom-part">
+            <div class="container-fluid">
+              <h2>Abbl</h2>
+              <h5 class="text-muted">Patryk Rzucidlo</h5>
+              <div class="col sm='8'">
+                <div class="row App-fill-containter">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     );
   }
 }
 
 export default App;
+
+/**
+ *         <Container fluid={false}>
+            <Col>
+            <div className="App-topBackground">
+              <img src={avatar} className="App-avatar" alt="avatar"/>
+             </div>
+            </Col>
+        </Container>
+        <Container className="App-bottom">
+
+        </Container>
+
+                  <div class="card App-card">
+                    <div class="card-body">
+                      <h5 class="card-title">ReactChat</h5>
+                      <p class="card-text">OpenSource chat written with React.js and Spring REST</p>
+                      <a href="https://www.github.com/abbl/ReactChat-front" class="btn btn-primary">Github</a>
+                    </div>
+                  </div>
+ */
